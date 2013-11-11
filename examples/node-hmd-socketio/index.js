338a04771786e11e136b33c6a3f137a7619a9214
GeoJosh <joshua.penton@geocent.com>
2013-11-11 15:13:01 -0600

    Updated examples.

--- examples/node-hmd-socketio/index.js
+++ examples/node-hmd-socketio/index.js
@@ -1,6 +1,7 @@
 var hmd = require("../.."),
 	express = require("express"),
-	http = require("http");
+	http = require("http"),
+	io = require("socket.io");
 
 // Create HMD manager object
 var manager = hmd.createManager("default");
@@ -31,6 +32,23 @@ app.get("/orientation", function (req, res) {
 });
 
 // Launch express server
-http.createServer(app).listen(app.get("port"), function () {
+var server = http.createServer(app).listen(app.get("port"), function () {
 	console.log("Express server listening on port " + app.get("port"));
 });
+
+// Attack socket.io listener to the server
+var socket = io.listen(server);
+
+// On socket connection set up event emitters to automatically push the HMD orientation data
+socket.sockets.on("connection", function (socket) {
+	var orientation = setInterval(function () {
+		manager.getDeviceOrientation(function(err, deviceOrientation) {
+			socket.emit("orientation", deviceOrientation);
+		});
+	}, 1000);
+
+	socket.on("disconnect", function () {
+		clearInterval(orientation);
+	});
+});
+
